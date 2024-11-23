92e031720c37ee15cdb8efc7ad5c367ebe740acf
Cyril Slabý <slaby.cyril@gmail.com>
2024-11-23 20:37:53 +0100

    Small fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { AuthModule } from './auth/auth.module';
 import { ListsModule } from './lists/lists.module';
 import { ItemsModule } from './items/items.module';
 import { AppDataSource } from './data-source'; // Import AppDataSource for TypeORM configuration
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -26,5 +28,7 @@ import { AppDataSource } from './data-source'; // Import AppDataSource for TypeO
     ListsModule, // Manages "Lists" functionality
     ItemsModule, // Manages "Items" within lists
   ],
+  controllers: [AppController], // Include AppController
+  providers: [AppService], // Include AppService
 })
 export class AppModule {}
